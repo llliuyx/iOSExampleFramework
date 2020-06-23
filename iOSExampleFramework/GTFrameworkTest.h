72d05f7e6364ff4994a5a86168d7c259c3d73c49
刘玉祥 <llliuyx@aliyun.com>
2020-06-23 19:03:39 +0800

    新增GTFrameworkTest

---
+++ iOSExampleFramework/GTFrameworkTest.h
@@ -0,0 +1,17 @@
+//
+//  GTFrameworkTest.h
+//  iOSExampleFramework
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GTFrameworkTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSExampleFramework/GTFrameworkTest.m
@@ -0,0 +1,13 @@
+//
+//  GTFrameworkTest.m
+//  iOSExampleFramework
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import "GTFrameworkTest.h"
+
+@implementation GTFrameworkTest
+
+@end
