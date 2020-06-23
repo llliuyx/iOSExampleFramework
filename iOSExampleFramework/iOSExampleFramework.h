ce99d11f37f6f738d0f9a113d6596627d4543f81
刘玉祥 <llliuyx@aliyun.com>
2020-06-23 18:45:40 +0800

    Initial Commit

---
+++ iOSExampleFramework/iOSExampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  iOSExampleFramework.h
+//  iOSExampleFramework
+//
+//  Created by liuyx on 2020/6/23.
+//  Copyright © 2020 liuyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSExampleFramework.
+FOUNDATION_EXPORT double iOSExampleFrameworkVersionNumber;
+
+//! Project version string for iOSExampleFramework.
+FOUNDATION_EXPORT const unsigned char iOSExampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSExampleFramework/PublicHeader.h>
+
+
